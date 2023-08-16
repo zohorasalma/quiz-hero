e2dacf615c647d211eba04db4ea134b8c8fbadee
zohorasalma <zohorasalma@gmail.com>
2023-08-16 21:29:10 +0600

    syntax error fixed in fetch

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
+ 
   displayQuiz(data);
 };
 
